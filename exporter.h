86df238ed0ba1427fd7f4efc3b1387a04688dc71
Francesco Balestrieri <bale@balenet.com>
2013-09-01 12:23:44 +0300

    Added license text

--- exporter.h
+++ exporter.h
@@ -1,3 +1,24 @@
+/*
+
+Copyright (C) 2012 Francesco Balestrieri
+
+This file is part of Timesheet for N9
+
+Timesheet for N9 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Timesheet for N9 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Timesheet for N9.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef EXPORTER_H
 #define EXPORTER_H
 
